65da990fd2d1d40c76ff463a4bc38cd6f733c3be
EmilyChi5 <emily.chi12232@gmail.com>
2026-04-19 20:52:37 -0500

    HW2 V2

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -15,7 +15,7 @@ void setServo(int angle)
 #define SERVO_PIN 16
 #define PWM_FREQ 50.0
 
-bool timer_interrupt_function(_unused struct repeating_timer *t)
+bool timer_interrupt_function(__unused struct repeating_timer *t)
 {
     // Read the adc
     uint16_t result1 = adc_read();
@@ -27,8 +27,47 @@ int main()
 {
     stdio_init_all();
 
+    // Using Timer Interrupt
+    struct repeating_timer timer;
+    add_repeating_timer_ms(-100, timer_interrupt_function, NULL, &timer);
+
+    // Turn on PWM
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM); // Set LED Pin to be PWM
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN); // Gets PWM Slice Number
+
+    // Divide by float from 1 to 255 until clock frequency is 150 MHz
+    float div = 50; 
+    pwm_set_clkdiv(slice_num, div); // Setting clock speed
+    uint16_t wrap = 6000; // Has to be less than 65535
+
+    // Set PWM Frequency and resolution
+    pwn_set_wrap(slice_num, wrap);
+    pwm_set_enabled(slice_num, true); // Turn on the PWM
+    pwm_set_gpio_level(SERVO_PIN, 0) // Sets duty cycle to 50%
+
+    // Turning on adc
+    adc_init();
+    adc_gpio_init(26)
+    adc_select_input(0)
+
     while (true) {
-        printf("Hello, world!\n");
+        // Loop through servo angles
+        int i = 0;
+
+        for (i = 10; i < 170; i++){
+            setServo(i);
+            sleep_ms(10);
+        }
+
+        for (i = 170; i > 10; i--){
+            setServo(i);
+            sleep_ms(10);
+        }
+
         sleep_ms(1000);
     }
+}
+
+void setServo (int angle){
+    pwm_set_gpio_level(SERVO_PIN, (int)((0.5)+(angle/180.0)*0.05)*6000);
 }
