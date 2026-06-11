f3a93aece88bbf407f39b24e8382af835ddeaa79
EmilyChi5 <emily.chi12232@gmail.com>
2026-06-10 20:32:32 -0400

    HW8
    Completed

--- HW8/HW8.c
+++ HW8/HW8.c
@@ -1,4 +1,4 @@
-// Base code copied over from HW7 assignment 
+// Base code copied over from HW7 assignment
 
 #include <stdio.h>
 #include <math.h>
@@ -13,13 +13,13 @@
 #define PIN_CS 17
 
 // New for RAM
-#define PIN_RX 16      // RAM SO pin
-#define PIN_RAM_CS 20  // RAM chip select
+#define PIN_RX 16     // RAM SO pin
+#define PIN_RAM_CS 20 // RAM chip select
 
 // 23K256 commands
 #define RAM_WRITE 0b00000010
-#define RAM_READ  0b00000011
-#define RAM_WRMR  0b00000001
+#define RAM_READ 0b00000011
+#define RAM_WRMR 0b00000001
 #define RAM_SEQ_MODE 0b01000000
 
 #define PI 3.14159265359
@@ -29,12 +29,14 @@ static inline void cs_deselect(uint cs_pin);
 
 void writeDAC(int channel, float V);
 void spi_ram_init(void);
-void ram_write_byte(uint16_t address, uint8_t data);
-uint8_t ram_read_byte(uint16_t address);
+void ram_write_bytes(uint16_t address, uint8_t *data, int len);
+void ram_read_bytes(uint16_t address, uint8_t *data, int len);
+void makeDACbytes(int channel, float V, uint8_t data[2]);
 
 int main()
 {
     stdio_init_all();
+    sleep_ms(3000);
 
     // Initalizing SPI Pins
     spi_init(spi_default, 1000 * 1000); // the baud, or bits per second
@@ -54,46 +56,69 @@ int main()
     // Initialize RAM
     spi_ram_init();
 
-    sleep_ms(1000);
+    sleep_ms(100);
 
-    ram_write_byte(0x0000, 0xAB);
-    uint8_t result = ram_read_byte(0x0000);
+    // sin values
+    for (int i = 0; i < 1000; i++)
+    {
+        float voltage = (sinf(2.0f * PI * i / 1000.0f) + 1.0f) / 2.0f * 3.3f;
 
-    printf("Read from RAM: 0x%02X\r\n", result);
+        uint8_t dac_data[2];
+        makeDACbytes(0, voltage, dac_data);
+
+        ram_write_bytes(2 * i, dac_data, 2);
+    }
+
+    printf("Finished writing sine wave to RAM\r\n");
+
+    // Shows some values to the serial monitor 
+    int test_samples[] = {0, 250, 500, 750};
+
+    for (int j = 0; j < 4; j++) {
+
+        uint8_t dac_data[2];
+
+        ram_read_bytes(2 * test_samples[j], dac_data, 2);
+
+        uint16_t command =
+            ((uint16_t)dac_data[0] << 8) |
+            dac_data[1];
+
+        uint16_t dac_value =
+            (command >> 2) & 0x03FF;
+
+        printf("Sample %d = %u\r\n",
+            test_samples[j],
+            dac_value);
+    }
 
     while (true)
     {
-        // // Making waves ranging from 0V to 3.3V
-        // // Basic Sine Wave
-        // float sine_voltage = (sinf(2.0f * PI * 2.0f *t) + 1.0f) / 2.0f * 3.3f;
+        for (int i = 0; i < 1000; i++)
+        {
+            uint8_t dac_data[2];
 
-        // float triangle_voltage;
-        // float phase = fmodf(t, 1.0f);
+            ram_read_bytes(2 * i, dac_data, 2);
 
-        // if (phase < 0.5f) {
-        //     triangle_voltage = phase * 2.0f *3.3f;
-        // } else {
-        //     triangle_voltage = (1.0f - phase) * 2.0f *3.3f;
-        // }
+            cs_select(PIN_CS);
+            spi_write_blocking(SPI_PORT, dac_data, 2);
+            cs_deselect(PIN_CS);
 
-        // writeDAC(0, sine_voltage); // Channel A
-        // writeDAC(1, triangle_voltage); // Channel B
-
-        // sleep_ms(5); // 200 Hz
-        // t += 0.005f;
-
-        sleep_ms(1000);
-
+            sleep_ms(1);
+        }
     }
 }
 
-void writeDAC(int channel, float V){
+void writeDAC(int channel, float V)
+{
 
-    if (V < 0.0f) {
+    if (V < 0.0f)
+    {
         V = 0.0f;
     }
 
-    if (V > 3.3f) {
+    if (V > 3.3f)
+    {
         V = 3.3f;
     }
 
@@ -114,7 +139,8 @@ void writeDAC(int channel, float V){
     cs_deselect(PIN_CS);
 }
 
-void spi_ram_init(void) {
+void spi_ram_init(void)
+{
     uint8_t data[2];
 
     data[0] = RAM_WRMR;
@@ -123,35 +149,6 @@ void spi_ram_init(void) {
     cs_select(PIN_RAM_CS);
     spi_write_blocking(SPI_PORT, data, 2);
     cs_deselect(PIN_RAM_CS);
-}
-
-void ram_write_byte(uint16_t address, uint8_t data) {
-    uint8_t buffer[4];
-
-    buffer[0] = RAM_WRITE;
-    buffer[1] = address >> 8;
-    buffer[2] = address & 0xFF;
-    buffer[3] = data;
-
-    cs_select(PIN_RAM_CS);
-    spi_write_blocking(SPI_PORT, buffer, 4);
-    cs_deselect(PIN_RAM_CS);
-}
-
-uint8_t ram_read_byte(uint16_t address) {
-    uint8_t buffer[3];
-    uint8_t data;
-
-    buffer[0] = RAM_READ;
-    buffer[1] = address >> 8;
-    buffer[2] = address & 0xFF;
-
-    cs_select(PIN_RAM_CS);
-    spi_write_blocking(SPI_PORT, buffer, 3);
-    spi_read_blocking(SPI_PORT, 0, &data, 1);
-    cs_deselect(PIN_RAM_CS);
-
-    return data;
 }
 
 static inline void cs_select(uint cs_pin)
@@ -168,4 +165,53 @@ static inline void cs_deselect(uint cs_pin)
     asm volatile("nop \n nop \n nop"); // FIXME
 }
 
+void ram_write_bytes(uint16_t address, uint8_t *data, int len)
+{
+    uint8_t header[3];
 
+    header[0] = RAM_WRITE;
+    header[1] = address >> 8;
+    header[2] = address & 0xFF;
+
+    cs_select(PIN_RAM_CS);
+    spi_write_blocking(SPI_PORT, header, 3);
+    spi_write_blocking(SPI_PORT, data, len);
+    cs_deselect(PIN_RAM_CS);
+}
+
+void ram_read_bytes(uint16_t address, uint8_t *data, int len)
+{
+    uint8_t header[3];
+
+    header[0] = RAM_READ;
+    header[1] = address >> 8;
+    header[2] = address & 0xFF;
+
+    cs_select(PIN_RAM_CS);
+    spi_write_blocking(SPI_PORT, header, 3);
+    spi_read_blocking(SPI_PORT, 0, data, len);
+    cs_deselect(PIN_RAM_CS);
+}
+
+void makeDACbytes(int channel, float V, uint8_t data[2])
+{
+    if (V < 0.0f)
+    {
+        V = 0.0f;
+    }
+
+    if (V > 3.3f)
+    {
+        V = 3.3f;
+    }
+
+    uint16_t myV = (uint16_t)(V / 3.3f * 1023.0f);
+
+    uint16_t command = 0;
+    command |= (channel & 0b1) << 15;
+    command |= 0b111 << 12;
+    command |= myV << 2;
+
+    data[0] = command >> 8;
+    data[1] = command & 0xFF;
+}
